95b04f80a48d129862de59e97439f10b4fa931a9
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-03-25 18:48:02 -0500

    feat: initialize spring boot project with spring initializr

---
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/SuperfrogSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperfrogSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperfrogSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/SuperfrogSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperfrogSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
