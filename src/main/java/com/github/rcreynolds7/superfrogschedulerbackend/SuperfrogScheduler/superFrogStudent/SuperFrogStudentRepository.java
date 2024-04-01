47985b10a404a06d0044fcf30db714a924a0cbc0
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-01 10:09:54 -0500

    feat: add SpiritDirector and SuperFrogStudent repository

---
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/superFrogStudent/SuperFrogStudentRepository.java
@@ -0,0 +1,8 @@
+package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler.superFrogStudent;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SuperFrogStudentRepository extends JpaRepository<SuperFrogStudent, Integer> {
+}
