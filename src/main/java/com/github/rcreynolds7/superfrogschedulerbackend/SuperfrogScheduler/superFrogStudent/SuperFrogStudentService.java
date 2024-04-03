424423fe35c0dfce40bcc776e33b591ab649bdb6
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-03 15:57:46 -0500

    feat: superfrogstudent findbyid and update operations in service file

---
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/superFrogStudent/SuperFrogStudentService.java
@@ -0,0 +1,38 @@
+package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler.superFrogStudent;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class SuperFrogStudentService {
+    private final SuperFrogStudentRepository superFrogStudentRepository;
+
+    @Autowired
+    public SuperFrogStudentService(SuperFrogStudentRepository superFrogStudentRepository) {
+        this.superFrogStudentRepository = superFrogStudentRepository;
+    }
+
+    public SuperFrogStudent findById(Integer superFrogStudentId) {
+        return this.superFrogStudentRepository.findById(superFrogStudentId).
+                orElseThrow(() -> new SuperFrogStudentNotFoundException(superFrogStudentId));
+    }
+
+    public SuperFrogStudent update(Integer superFrogStudentId, SuperFrogStudent update) {
+        return this.superFrogStudentRepository.findById(superFrogStudentId)
+                .map(oldSuperFrogStudent -> {
+                    oldSuperFrogStudent.setFirstName(update.getFirstName());
+                    oldSuperFrogStudent.setLastName(update.getLastName());
+                    oldSuperFrogStudent.setEmail(update.getEmail());
+                    oldSuperFrogStudent.setPhone(update.getPhone());
+                    oldSuperFrogStudent.setProfileInfo(update.getProfileInfo());
+                    oldSuperFrogStudent.setActive(update.getActive());
+
+                    return this.superFrogStudentRepository.save(oldSuperFrogStudent);
+                })
+                .orElseThrow(() -> new SuperFrogStudentNotFoundException(superFrogStudentId));
+    }
+}
