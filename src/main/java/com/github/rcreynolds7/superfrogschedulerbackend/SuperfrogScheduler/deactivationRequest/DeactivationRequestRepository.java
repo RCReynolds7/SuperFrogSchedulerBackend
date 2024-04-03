65e93b3748f1e5ec1b77d9db10c041195f7a283d
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-03 15:35:29 -0500

    refactor: remove deactivation request entity

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/deactivationRequest/DeactivationRequestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler.deactivationRequest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeactivationRequestRepository extends JpaRepository<DeactivationRequest, Integer> {
-}
