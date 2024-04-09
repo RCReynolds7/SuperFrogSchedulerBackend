e2cfa17bfddcfdfc88a52af4ca56d45b45047348
jennabuzby1 <146042997+jennabuzby1@users.noreply.github.com>
2024-04-08 19:00:22 -0500

    Add files via upload

---
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/RequestStatus.java
@@ -0,0 +1,13 @@
+package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler.SuperFrogStudent;
+
+public enum RequestStatus {
+
+    COMPLETED,
+    PENDING,
+
+    REJECTED,
+    APPROVED,
+
+    CANCELLED
+
+}
