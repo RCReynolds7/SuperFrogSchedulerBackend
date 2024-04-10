e661cf642dbefb1ae8165f667493e7780fc84dc9
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-10 11:45:39 -0500

    fix: only update attributes with existing fields in update

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/superFrogStudent/SuperFrogStudentController.java
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/superFrogStudent/SuperFrogStudentController.java
@@ -14,9 +14,46 @@ public class SuperFrogStudentController {
         this.superFrogStudentService = superFrogStudentService;
     }
 
-    @PutMapping("/{id}")
-    public Result updateSuperFrogStudent(@PathVariable Integer id, @RequestBody SuperFrogStudent superFrogStudentUpdate) {
-        SuperFrogStudent updatedSuperFrogStudent = superFrogStudentService.update(id, superFrogStudentUpdate);
+    @PutMapping("/{superFrogStudentId}")
+    public Result updateSuperFrogStudent(@PathVariable Integer superFrogStudentId, @RequestBody SuperFrogStudent superFrogStudentUpdate) {
+        SuperFrogStudent superFrogStudent = superFrogStudentService.findById(superFrogStudentId);
+
+        if (superFrogStudentUpdate.getFirstName() != null && !superFrogStudentUpdate.getFirstName().isEmpty()) {
+            superFrogStudent.setFirstName(superFrogStudentUpdate.getFirstName());
+        }
+
+        if (superFrogStudentUpdate.getLastName() != null && !superFrogStudentUpdate.getLastName().isEmpty()) {
+            superFrogStudent.setLastName(superFrogStudentUpdate.getLastName());
+        }
+
+        if (superFrogStudentUpdate.getEmail() != null && !superFrogStudentUpdate.getEmail().isEmpty()) {
+            superFrogStudent.setEmail(superFrogStudentUpdate.getEmail());
+        }
+
+        if (superFrogStudentUpdate.getPhone() != null && !superFrogStudentUpdate.getPhone().isEmpty()) {
+            superFrogStudent.setPhone(superFrogStudentUpdate.getPhone());
+        }
+
+        if (superFrogStudentUpdate.getAddress() != null && !superFrogStudentUpdate.getAddress().isEmpty()) {
+            superFrogStudent.setAddress(superFrogStudentUpdate.getAddress());
+        }
+
+        if (superFrogStudentUpdate.getActive() != null) {
+            superFrogStudent.setActive(superFrogStudentUpdate.getActive());
+        }
+
+        if (superFrogStudentUpdate.getInternational() != null) {
+            superFrogStudent.setInternational(superFrogStudentUpdate.getInternational());
+        }
+
+        if (superFrogStudentUpdate.getPaymentPreference() != null) {
+            superFrogStudent.setPaymentPreference(superFrogStudentUpdate.getPaymentPreference());
+        }
+
+        // Update the entity in the database
+        superFrogStudentService.update(superFrogStudentId, superFrogStudent);
+
+
         return new Result(true, StatusCode.SUCCESS, "SuperFrog Student information updated successfully");
     }
 }
