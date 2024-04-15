ca6cf4d3aa084ed501fed874dc1c12b02b73389a
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-15 18:09:38 -0500

    refactor: remove unused file

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/superFrogStudent/SuperFrogStudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler.superFrogStudent;
-
-public class SuperFrogStudentNotFoundException extends RuntimeException {
-    public SuperFrogStudentNotFoundException(Integer id) {
-        super("Could not find SuperFrog student with Id " + id);
-    }
-}
