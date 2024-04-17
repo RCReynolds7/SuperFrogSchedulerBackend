a7f31cfe824e9946368418116d530185b7dba737
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-16 19:25:18 -0500

    feat: findByAssignedSuperFrogStudentAndAppearanceRequestStatusInAndDateBetween

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/appearanceRequest/AppearanceRequestRepository.java
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/appearanceRequest/AppearanceRequestRepository.java
@@ -5,10 +5,17 @@ import com.github.rcreynolds7.superfrogschedulerbackend.SuperfrogScheduler.syste
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Repository
 public interface AppearanceRequestRepository extends JpaRepository<AppearanceRequest, Integer> {
     List<AppearanceRequest> findByAssignedSuperFrogStudentAndAppearanceRequestStatusIn(SuperFrogStudent student, List<AppearanceRequestStatus> statuses);
+    List<AppearanceRequest> findByAssignedSuperFrogStudentAndAppearanceRequestStatusInAndDateBetween(
+            SuperFrogStudent student,
+            List<AppearanceRequestStatus> statuses,
+            LocalDateTime startDate,
+            LocalDateTime endDate
+    );
 }
