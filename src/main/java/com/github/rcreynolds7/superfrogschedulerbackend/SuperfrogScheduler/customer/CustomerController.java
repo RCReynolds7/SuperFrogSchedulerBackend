156c7013f1b6002a88e76e349ccfcd726a2659dc
jennabuzby1 <146042997+jennabuzby1@users.noreply.github.com>
2024-04-17 21:22:22 -0500

    Added the changeable options to CustomerController

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/customer/CustomerController.java
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/customer/CustomerController.java
@@ -28,7 +28,6 @@ public class CustomerController {
         // Create appearance request service in the database
         AppearanceRequest savedAppearanceRequest = this.appearanceRequestService.save(appearanceRequest);
 
-
         return new Result(true, StatusCode.SUCCESS, "SuperFrog appearance request created successfully.", savedAppearanceRequest);
     }
 
@@ -37,19 +36,64 @@ public class CustomerController {
         AppearanceRequest appearanceRequest = appearanceRequestService.findById(appearanceRequestId);
 
         Optional.ofNullable(appearanceRequestUpdate.getFirstName())
-                .filter( name -> !name.isEmpty())
+                .filter(name -> !name.isEmpty())
                 .ifPresent(appearanceRequest::setFirstName);
 
         Optional.ofNullable(appearanceRequestUpdate.getLastName())
-                .filter( name -> !name.isEmpty())
+                .filter(name -> !name.isEmpty())
                 .ifPresent(appearanceRequest::setLastName);
 
         Optional.ofNullable(appearanceRequestUpdate.getEmail())
-                .filter( email -> !email.isEmpty())
+                .filter(email -> !email.isEmpty())
                 .ifPresent(appearanceRequest::setEmail);
 
-    }
+        Optional.ofNullable(appearanceRequestUpdate.getPhone())
+                .filter(phone -> !phone.isEmpty())
+                .ifPresent(appearanceRequest::setPhone);
 
+        Optional.ofNullable(appearanceRequestUpdate.getEventAddress())
+                .filter(eventAddress -> !eventAddress.isEmpty())
+                .ifPresent(appearanceRequest::setEventAddress);
+
+        Optional.ofNullable(appearanceRequestUpdate.getDate())
+                .ifPresent(appearanceRequest::setDate);
+
+        Optional.ofNullable(appearanceRequestUpdate.getTypeOfEvent())
+                .filter(typeOfEvent -> !typeOfEvent.isEmpty())
+                .ifPresent(appearanceRequest::setTypeOfEvent);
+
+        Optional.ofNullable(appearanceRequestUpdate.getEventTitle())
+                .filter(eventTitle -> !eventTitle.isEmpty())
+                .ifPresent(appearanceRequest::setEventTitle);
+
+        Optional.ofNullable(appearanceRequestUpdate.getNameOfOrg())
+                .filter(nameOfOrg -> !nameOfOrg.isEmpty())
+                .ifPresent(appearanceRequest::setNameOfOrg);
+
+        Optional.ofNullable(appearanceRequestUpdate.getIsOnCampus())
+                .filter(isOnCampus -> !isOnCampus.isEmpty())
+                .ifPresent(appearanceRequest::setIsOnCampus);
+
+        Optional.ofNullable(appearanceRequestUpdate.getSpecialInstructions())
+                .filter(specialInstructions -> !specialInstructions.isEmpty())
+                .ifPresent(appearanceRequest::setSpecialInstructions);
+
+        Optional.ofNullable(appearanceRequestUpdate.getExpensesOrBenefits())
+                .filter(expensesOrBenefits -> !expensesOrBenefits.isEmpty())
+                .ifPresent(appearanceRequest::setExpensesOrBenefits);
+
+        Optional.ofNullable(appearanceRequestUpdate.getOtherOrganizationsInvolved())
+                .filter(otherOrganizationsInvolved -> !otherOrganizationsInvolved.isEmpty())
+                .ifPresent(appearanceRequest::setOtherOrganizationsInvolved);
+
+        Optional.ofNullable(appearanceRequestUpdate.getDetailedEventDescription())
+                .filter(detailedEventDescription -> !detailedEventDescription.isEmpty())
+                .ifPresent(appearanceRequest::setDetailedEventDescription);
+
+        appearanceRequestService.updateAppearanceRequest(appearanceRequestId, appearanceRequest);
+
+        return new Result(true, StatusCode.SUCCESS, "Appearance request information updated successfully");
+    }
 }
 
 
