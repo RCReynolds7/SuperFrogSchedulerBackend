24868255ea65577ab1f356688afd1c9db7b3eaeb
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-21 21:12:43 -0500

    fix: allow  WebConfig.java to allow all origins, HTTP methods, and headers

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/config/WebConfig.java
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/config/WebConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
