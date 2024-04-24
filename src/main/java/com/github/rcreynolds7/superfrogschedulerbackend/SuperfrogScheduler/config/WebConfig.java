011ebec23f803632d0a4d9c2eb37008504247a22
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-23 23:22:57 -0500

    fix: update cors allowance

--- src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/config/WebConfig.java
+++ src/main/java/com/github/rcreynolds7/superfrogschedulerbackend/SuperfrogScheduler/config/WebConfig.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Allow this origin to make requests
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify the allowed methods
+                .allowedHeaders("*"); // Allow all headers
+
     }
 }
